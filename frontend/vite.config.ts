b6d407be9886c0c96b1ab531725e82b78a0d3b7c
ZebVert420 <elliott.og11@gmail.com>
2025-03-25 18:23:50 -0400

    Configuration GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Kote-Piscine-WebSite/', // Remplacez par le nom de votre dépôt
 })
